c86236187c40065169e0cd393dae2eb94603ce78
Nitesh Sapkota <lionitesh10@gmail.com>
2022-10-17 07:00:43 +0545

    Added a test change

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
+    # This is a change
     return render_template('index.html')
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
